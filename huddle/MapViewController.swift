26d5a2c9f1443be503afa26dbd431601fe797ab5
Mateo Garcia <mateog@stanford.edu>
2017-03-19 19:33:52 -0700

    Initialize.

---
+++ huddle/MapViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MapViewController.swift
+//  huddle
+//
+//  Created by Mateo Garcia on 3/19/17.
+//  Copyright © 2017 Mateo Garcia. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    let METERS_PER_MILE = 1609.344
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.blurView.alpha = 0.6
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        var zoomLocation = CLLocationCoordinate2D()
+        zoomLocation.latitude = 37.769319
+        zoomLocation.longitude = -122.487104
+        
+        let viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5 * METERS_PER_MILE, 0.5 * METERS_PER_MILE)
+        self.mapView.setRegion(viewRegion, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
